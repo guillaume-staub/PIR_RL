fadb2cca26085226b3780b11b2ac63d1451cfa8a
guillaume-staub <guillaume.st0b@gmail.com>
2025-03-17 16:14:33 +0100

    Adding the config.yaml file to manage functions

--- environnement.py
+++ environnement.py
@@ -9,72 +9,89 @@ import gymnasium as gym
 import numpy as np
 import pandas as pd
 from stable_baselines3.common.env_checker import check_env
+import yaml
 
+# Loading of the YAML file:
+def load_config(yaml_file):
+    with open(yaml_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-    #### renewable ninja
+config = load_config('config.yaml')
+reward_function = config["reward_function"]
+update_levels_function = config["update_levels_function"]
+
+print(f"Reward function: {reward_function}")
+print(f"Step function: {update_levels_function}")
+
 
 class CustomEnv(gym.Env):
     def __init__(self):
         super(CustomEnv, self).__init__()
 
         # Define action and observation space
-        # Example: Continuous action space with 1 dimension
         self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
-        #0 ne pas toucher au PHS, -1 se vider de la plus haute valeur possible, 1 stocker de la plus haute valeur possible
-        # self.action_space = gym.spaces.Box(low=np.array([min1,min2]), high=np.array([max1,max2]), shape(2,), dtype=np.float32) #si besoin de plus que une action
+        # Action meaning:
+            #   0: Do nothing with the PHS
+            #  -1: Use the PHS as much as we can to provide the demand of electricity
+            #   1: Store as much energy as possible in the PHS
+        # self.action_space = gym.spaces.Box(low=np.array([min1,min2]), high=np.array([max1,max2]), shape(2,), dtype=np.float32) # if we need more than one action
+
 
         # Collection of the data
-        demand_data=pd.read_csv('./data/demand2050_ADEME.csv', header=None)
+
+        demand_data = pd.read_csv('./data/demand2050_ADEME.csv', header=None)
         demand_data.columns = ["time","demand"]
-        self.times=demand_data['time'].values
-        self.time=0 #curseur de temps
-        self.demand=demand_data['demand'].values
-        self.solar_data=pd.read_csv('./data/solar.csv')['facteur_charge'].values
-        self.wind_data=pd.read_csv('./data/wind_onshore.csv')['facteur_charge'].values
+        self.times = demand_data['time'].values
+        self.time = 0   # Time indicator
+        self.demand = demand_data['demand'].values
+        self.solar_data = pd.read_csv('./data/solar.csv')['facteur_charge'].values
+        self.wind_data = pd.read_csv('./data/wind_onshore.csv')['facteur_charge'].values
+
 
         # definition of the variables of the environment
 
-        self.nb_furnished_demand=0
+        self.nb_furnished_demand = 0
        
-        self.wind_capacity = 170.1     # given ? or to be calculated ? or to be set with different values to test ?
-        self.solar_capacity = 308.4    # max of energy we can gather using wind / sun in one step
+        self.wind_capacity = 170.1  # given ? or to be calculated ? or to be set with different values to test ?
+        self.solar_capacity = 308.4 # max of energy we can gather using wind / sun in one step
 
-        self.phs_capacity=180
-        self.phs_power=9.3
-        self.phs_efficiency=0.75
+        self.phs_capacity = 180
+        self.phs_power = 9.3
+        self.phs_efficiency = 0.75
         
-        self.gas_capacity=125000
-        self.gas_power_in=7.66
-        self.gas_power_out=32.93
-        self.gas_efficiency=0.4
+        self.gas_capacity = 125000
+        self.gas_power_in = 7.66
+        self.gas_power_out = 32.93
+        self.gas_efficiency = 0.4
 
         # Box observation space with 5 dimensions
         #                                                     time  date   residual prod    phs level   gaz level
         self.observation_space = gym.spaces.Box(low=np.array( [0,   0,    -1,               0,          0]), 
                                                 high=np.array([1,   1,     1,               1,          1]),
                                                 dtype=np.float64)
-
         self.state = None
-        # Self observation space : le vecteur des états (date heure niveau phs etc)
-        # Self state : pour initialiser dès le premier tour les variables, habituellement mis a None
-        info,obs=self.reset()
-      
+        # Self observation space : State vector (date time PHS levels and so on)
+        # Self state : Usually at None, used to initialize values at the very first round
+        info, obs=self.reset()
+
+
     def reset(self,seed=None,options=None):
         """Reset the environment to its initial state"""
-        self.time=0 # np.random.choice(self.times)
+        self.time = 0   # np.random.choice(self.times)
         self.total_reward = 0
         residual_production=self.wind_capacity*self.wind_data[self.time] + self.solar_capacity*self.solar_data[self.time] - self.demand[self.time]
-        
-        #on commence à une heure et un jour aléatoire de l'année, avec la production résiduel de ce jour et les réservoirs à moitié pleins
-        self.state = np.array([self.time%24/24,int(self.time/24)%365/365,residual_production/(self.wind_capacity+self.solar_capacity),1/2,1/2]) # Voir hypothèses (stock au milieu de la capa max)
-        # faire l'histogramme des valeurs, voir si la normalisation est pertinente
+
+        # We start at a random day and hour of the year, with the residual production of the year, and the energy tanks half full
+        self.state = np.array([self.time%24/24,int(self.time/24)%365/365,residual_production/(self.wind_capacity+self.solar_capacity),1/2,1/2])
+
+        # TO DO : the histogram of the values to see if normalization is pertinent
         obs=self.state
         info=dict()
         return obs,info
     
-    
-    
-    def reward(self,no_furnished_demand):
+
+    def reward_v1(self,no_furnished_demand):
         if no_furnished_demand>0 :
             reward=-1
         else :
@@ -82,14 +99,15 @@ class CustomEnv(gym.Env):
         return reward
 
 
-
-    def level_reward(self,no_furnished_demand):
+    def reward_v2(self,no_furnished_demand):
         if no_furnished_demand>0 :
             reward=-10
         else :
             reward=self.state[3]*self.phs_capacity/self.gas_capacity+self.state[4]
-        return reward  
+        return reward 
     
+    
+
 
 
 ###############################################################   U P D A T E   L E V E L   1   #####################################################################################################
@@ -106,14 +124,12 @@ class CustomEnv(gym.Env):
                                                             # Sinon, on fournit autant qu'on peut avec le Gaz pour satisfaire la demande.
                                                             # Si le Gaz suffit, on utilise aussi du Gaz pour remplir les PHS
                                                             # Sinon on vide les PHS pour fournir la demande
-            
-            
+           
             if (residual_production > qty_asked_for_phs):
                 phs_in = min((1 - self.state[3])*self.phs_capacity, qty_asked_for_phs)
                 gas_in = (residual_production - phs_in) #On ajoutera les contraintes à la fin
                 
             else:
-                
                 gas_usage = min(qty_asked_for_phs - residual_production, self.gas_power_out, self.state[4]*self.gas_efficiency, (1 - self.state[3])*self.phs_capacity-residual_production) #qté de gaz à sortir pour remplir les phs en complément de la prod résiduelle
                     # La quantité de Gaz que l'on va vider pour remplir les PHS et la demande
                 
@@ -123,32 +139,34 @@ class CustomEnv(gym.Env):
                 else :#ici on le force à vider pour répondre à la demande, j'ai peur que l'agent comprenne pas ce qui se passe => ne rien faire plutot et laisser une demande pas remplie ????
                     phs_usage=min(-(residual_production+gas_usage), self.phs_power, self.state[3]*self.phs_efficiency)
                     
-        else : # on vide les phs
+        else: # on vide les phs
             phs_usage=min(-self.phs_power*action[0], self.state[3]*self.phs_efficiency*self.phs_capacity)
 
             if residual_production+phs_usage>0 : #on rempli le gaz
                 gas_in=phs_usage+residual_production
+
             else : #on vide aussi du gaz pour essayer de répondre à la demande
                 gas_usage=min(self.state[4]*self.gas_capacity*self.gas_efficiency,self.gas_power_out,-(residual_production+phs_usage))
             
-        #ajout des contraintes
+        # Adding the Constraints
         phs_in=min(phs_in,self.phs_power,self.phs_capacity-self.state[3]*self.phs_capacity)
         gas_in=min(gas_in,self.gas_power_in,self.gas_capacity-self.state[4]*self.gas_capacity)
         
-        #mise à jour des etats
+        # Update of the capacity levels
         state3=self.state[3]+phs_in/self.phs_capacity-phs_usage/self.phs_efficiency/self.phs_capacity
         state4=self.state[4]+gas_in/self.gas_capacity-gas_usage/self.gas_efficiency/self.gas_capacity
         
-        #calcul demande non fournie
+        # Computation of the unfurnished demand
         no_furnished_demand=max(0,-residual_production-gas_usage-phs_usage)
         
-                                                    #pour des tests unitaires
+        # For unit tests
         return (state3,state4,no_furnished_demand, phs_in, gas_in, phs_usage, gas_usage)
     
 
 ###############################################################   U P D A T E   L E V E L   2   #####################################################################################################
-    def update_levels_unguided(self,action,residual_production):  #on essaye de faire ce que veut l'agent, seules les limites physiques le contraignent
-
+    
+    # We do as the agent says, only physical limits can override its action
+    def update_levels_unguided(self,action,residual_production):
         phs_in=0 
         gas_in=0 
         phs_usage=0 
@@ -162,11 +180,8 @@ class CustomEnv(gym.Env):
                 gas_in = (residual_production - qty_asked_for_phs) #perte d'énergie si l'agent veut trop remplir les phs
                      
             else:
-                
                 gas_usage = min(qty_asked_for_phs - residual_production, self.gas_power_out, self.state[4]*self.gas_efficiency)#, (1 - self.state[3])*self.phs_capacity-residual_production) #qté de gaz à sortir pour remplir les phs en complément de la prod résiduelle
                 # La quantité de Gaz que l'on va vider pour remplir les PHS et la demande
-                
-                
                 phs_in=min(qty_asked_for_phs, gas_usage, (1 - self.state[3])*self.phs_capacity) #on ajoutera les contraintes à la fin
                 no_furnished_demand=max(0,gas_usage-phs_in+residual_production)
          
@@ -192,6 +207,20 @@ class CustomEnv(gym.Env):
         return (state3,state4,no_furnished_demand, phs_in, gas_in, phs_usage, gas_usage)
 
 
+    reward_functions = {
+        "reward_v1": reward_v1,
+        "reward_v2": reward_v2
+    }
+    
+    update_levels_functions = {
+    "update_levels_guided": update_levels_guided,
+    "update_levels_unguided": update_levels_unguided
+    }
+
+    reward_function = reward_functions[reward_function]
+    update_level_function = update_levels_functions[update_levels_function]
+
+
     def step(self, action):
         """Take an action and return the new state, reward, done, and info"""
         assert self.action_space.contains(action), "Invalid action!"
@@ -212,7 +241,7 @@ class CustomEnv(gym.Env):
       
         self.state[2] = residual_production/(self.wind_capacity+self.solar_capacity) #taux par rapport à la production maximale possible, majoré par 1 mais peut être inférieure à -1 si la demande est deux fois plus importante que l'offre
         
-        self.state[3],self.state[4],no_furnished_demand,phs_in, gas_in, phs_usage, gas_usage=self.update_levels_guided(action,residual_production)
+        self.state[3],self.state[4],no_furnished_demand,phs_in, gas_in, phs_usage, gas_usage=self.update_level_function(action,residual_production)
         
         if no_furnished_demand==0 :
             self.nb_furnished_demand+=1
@@ -223,7 +252,7 @@ class CustomEnv(gym.Env):
         #tests sur les bornes des états
         assert (self.state[0]>=0 or self.state[0] <= 1), "L'état 0 est hors des limites définies !"
         assert (self.state[1]>=0 or self.state[1] <= 1), "L'état 1 est hors des limites définies !"
-        assert (self.state[2] <= 1), "L'état 2 est hors des limites définies !"
+        assert (self.state[2]<=1), "L'état 2 est hors des limites définies !"
         assert (self.state[3]>=0 or self.state[3] <= 1), "L'état 3 est hors des limites définies !"
         assert (self.state[4]>=0 or self.state[4] <= 1), "L'état 4 est hors des limites définies !"
         
@@ -241,7 +270,7 @@ class CustomEnv(gym.Env):
         # Reward function
         # cout basé sur la pénalité d'écrétage / de perte par conversion d'énergie ?
       
-        reward=self.level_reward(no_furnished_demand)
+        reward = self.reward_function(no_furnished_demand)
         self.total_reward += reward
 
         # Example: Termination condition
