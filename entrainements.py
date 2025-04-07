8f58f19648b8a44d7dafd8990104bf589708a05b
ElsaEhr <e.ehr@net-c.com>
2025-04-07 21:29:57 +0200

    fonction train

--- entrainements.py
+++ entrainements.py
@@ -10,29 +10,33 @@ from stable_baselines3 import SAC
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback
 import time
+import yaml
 
+def train(reward,nb_iter,save_freq,name,config_path="./config.yaml") :
+    
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    config['reward_function'] = reward
+    with open(config_path, 'w') as file:
+        yaml.safe_dump(config, file)
+    
+    # Wrapping the environment for logging
+    env = CustomEnv()
+    env = Monitor(env)  # Pour enregistrer les logs de performance
+    # SAC model
+    model = SAC("MlpPolicy", env, verbose=1)#, learning_rate=0.0003, gamma=0.99, buffer_size=1000000, batch_size=256, train_freq=1)
+    # Callback pour sauvegarder le modèle pendant l'entraînement
+    checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path="./models/", name_prefix="tmp_save")
+    start=time.time()
+    # Training the agent
+    model.learn(total_timesteps=nb_iter, callback=checkpoint_callback)
+    # Save the final model
+    model.save("./models/"+name)
+    end=time.time()
+    
+    print("name")
+    print("time elapsed")
+    print(end-start)
 
-
-# Wrapping the environment for logging
-env = CustomEnv()
-env = Monitor(env)  # Pour enregistrer les logs de performance
-
-# SAC model
-model = SAC("MlpPolicy", env, verbose=1)#, learning_rate=0.0003, gamma=0.99, buffer_size=1000000, batch_size=256, train_freq=1)
-
-# Callback pour sauvegarder le modèle pendant l'entraînement
-checkpoint_callback = CheckpointCallback(save_freq=300000, save_path="./models/", name_prefix="sac_model")
-
-
-start=time.time()
-
-# Training the agent
-model.learn(total_timesteps=300000, callback=checkpoint_callback)
-
-# Save the final model
-model.save("./models/2years_guided_reward_end_and_hours_3_10e5")
-
-end=time.time()
-
-print(end-start)
-
+train("reward_v1",100,100,"test")
+#2years_guided_reward_weekly_and_hours_3_10e5
